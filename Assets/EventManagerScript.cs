0ddbe224255da43ea4a264da38912644521fe5ea
cynw22 <cynthia.wang218@gmail.com>
2025-11-03 00:19:12 -0500

    Added Menu Scene, Settings, and Credits

---
+++ Assets/EventManagerScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class EventManagerScript : MonoBehaviour
+{
+    public void PlayGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SettingsPage()
+    {
+        SceneManager.LoadScene("SettingsScene");
+    }
+
+    public void CreditsPage() {
+        SceneManager.LoadScene("CreditsScene");
+    
+    }
+        
+}
