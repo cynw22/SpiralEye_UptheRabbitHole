e5c79d1d6c9843fefa2c9e209899084fdff470b8
eldritchred <kaala.lily@gmail.com>
2025-11-07 09:07:41 -0500

    beginning lv1 puzzle control

---
+++ Assets/Scripts/PuzzleControl_1.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PuzzleControl_1 : MonoBehaviour
+{
+    // PUZZLE #1 - WORD SCRAMBLE CHEST
+    public bool chestIsOpen;
+    public string chestPasscode;
+
+    // PUZZLE #2 - DRINK ME BOTTLES
+    public int numBottles;
+    public int numSafeBottles;
+    public bool[] bottleLocations;
+    public int[] safeBottleLocationIndex;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // PUZZLE #1 - WORD SCRAMBLE PUZZLE
+        chestIsOpen = false;
+        chestPasscode = "SPIRE";
+
+        // PUZZLE #2 - DRINK ME BOTTLES
+        numBottles = 9;
+        numSafeBottles = 4;
+        bottleLocations = new bool[numBottles];
+        safeBottleLocationIndex = new int[numSafeBottles];
+        setSafeBottleLocations();
+    }
+
+    // PUZZLE #1 - WORD SCRAMBLE CHEST
+    // take player's attempted passcode, compare to chest passcode. if same, open chest
+    public bool tryPasscode(string passcode)
+    {
+        if (chestPasscode.Equals(passcode))
+        {
+            chestIsOpen = true;
+        }
+
+        return chestIsOpen;
+    }
+
+    // PUZZLE #2 - DRINK ME BOTTLES
+    // determine which locations will hold safe bottles
+    public void setSafeBottleLocations()
+    {
+        for (int i = 0; i < safeBottleLocationIndex.Length; i++)
+        {
+
+        }
+    }
+}
