418ea7f27629b0fc38db210e52b6702e771ea509
AkiSuns <akhilasundaram@cmail.carleton.ca>
2025-11-14 00:58:47 -0500

    Add the music!

---
+++ Assets/Scripts/SoundFXManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundFXManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource soundFxObject;
+
+    private void Awake()
+    {
+        soundFxObject = GetComponent<AudioSource>();
+        if (soundFxObject == null)
+        {
+            soundFxObject = gameObject.AddComponent<AudioSource>();
+            if (soundFxObject == null)
+            {
+                soundFxObject = gameObject.AddComponent<AudioSource>();
+            }
+        }
+    }
+
+    //play single clip
+    public void PlaySound(AudioClip clip, float volume = 1f)
+    {
+        if (clip == null) return;
+
+        soundFxObject.PlayOneShot(clip, volume);
+    }
+
+    //play random clip
+    public void PlayRandomSound(AudioClip[] clips, float volume = 1f)
+    {
+        if (clips == null || clips.Length == 0) return;
+        int index = Random.Range(0, clips.Length);
+        PlaySound(clips[index], volume);
+    }
+
+    // Start looping a clip
+    public void PlayLoop(AudioClip clip)
+    {
+        if (soundFxObject.clip == clip && soundFxObject.isPlaying) return;
+        soundFxObject.clip = clip;
+        soundFxObject.loop = true;
+        soundFxObject.Play();
+    }
+
+    // Stop looping clip
+    public void StopLoop()
+    {
+        soundFxObject.Stop();
+        soundFxObject.clip = null;
+        soundFxObject.loop = false;
+    }
+}
+
