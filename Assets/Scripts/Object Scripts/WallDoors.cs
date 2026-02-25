3eea42cc795944e92f86c043f5abf49dd6dac517
eldritchred <kaala.lily@gmail.com>
2026-02-25 14:52:46 -0500

    wall doors functional

---
+++ Assets/Scripts/Object Scripts/WallDoors.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class WallDoors : MonoBehaviour
+{
+    [SerializeField] public SwitchManager switchManager;
+
+    [SerializeField] public GameObject item;
+    [SerializeField] public Sprite a_sprite;
+    [SerializeField] public Sprite c_sprite;
+
+    public SpriteRenderer spriteRenderer;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+        item.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRenderer.sprite = switchManager.sisterPOV ? c_sprite : a_sprite;
+        if (!switchManager.sisterPOV) { item.SetActive(false); }
+        else { item.SetActive(true); }
+    }
+
+    private void OnMouseDown()
+    {
+        spriteRenderer.enabled = !spriteRenderer.enabled;
+        if (spriteRenderer.enabled && switchManager.sisterPOV)
+        {
+            item.SetActive(true);
+        }
+        else
+        {
+            item.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/SwitchUI.cs
+++ Assets/Scripts/SwitchUI.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SwitchPopup : MonoBehaviour
+public class SwitchUI : MonoBehaviour
 {
     public SwitchManager switchManager;
     public Sprite aliceSprite;
