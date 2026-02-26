4b3511a1a1f800b2186adae02b96c395a32beba3
AkiSuns <akhilasundaram@cmail.carleton.ca>
2026-02-26 00:45:30 -0500

    Revert "Merge branch 'main' of https://github.com/cynw22/SpiralEye_UptheRabbitHole"
    This reverts commit 791ace40905f0752ae07864328b22b98a4c5fe09, reversing
    changes made to f99089263f3ce8adec5161620428956461abbb2e.

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -1,20 +1,13 @@
-using JetBrains.Annotations;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 // Referenced from https://www.youtube.com/watch?v=JybecYyj83k
 
-public class CursorManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+public class CursorManager : MonoBehaviour
 {
     public SwitchManager switchManager;
     public Sprite aliceSprite;
-    public Sprite alicePointerSprite;
-    public Sprite aliceShapeSprite;
     public Sprite constanceSprite;
-    public Sprite constancePointerSprite;
-    public Sprite constanceShapeSprite;
 
     private Vector3 hotspot = new Vector3(35.0f, -40.0f, 0.0f);
     private Image image;
@@ -31,29 +24,6 @@ public class CursorManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     void Update()
     {
         transform.position = Input.mousePosition + hotspot;
-    }
-
-    //change to hand when hovering over buttons
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        image.sprite = switchManager.sisterPOV == false ? alicePointerSprite : constancePointerSprite;
-    }
-
-    //change back to regular cursor when leaving
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        image.sprite = switchManager.sisterPOV == false ? aliceSprite : constanceSprite;
-    }
-
-    //change to shape sprite when mouse is down
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        image.sprite = switchManager.sisterPOV == false ? aliceShapeSprite : constanceShapeSprite;
-    }
-
-    //change back to regular cursor when click is released
-    public void OnPointerUp(PointerEventData eventData)
-    {
         image.sprite = switchManager.sisterPOV == false ? aliceSprite : constanceSprite;
     }
 }
