425caa5a4927187e70ac7062c5a053d360a34af3
cynw22 <cynthia.wang218@gmail.com>
2026-03-27 23:39:56 -0400

    OfficeConWorks

--- Assets/WhiteRabbitRoomScript.cs
+++ Assets/WhiteRabbitRoomScript.cs
@@ -8,10 +8,23 @@ using UnityEngine.InputSystem;
 
 public class WhiteRabbitRoomScript : MonoBehaviour
 {
+    //Lobby
     [SerializeField] GameObject background_0;
+ 
+    //Constance
     [SerializeField] GameObject background_1;
     [SerializeField] GameObject background_2;
+   
+    //Alice Secret Room
     [SerializeField] GameObject background_3;
+
+    //Background1
+    [SerializeField] GameObject background_4;
+    
+    //Background2
+    [SerializeField] GameObject background_5;
+
+
     [SerializeField] bool isAlice;
 
 
@@ -27,24 +40,41 @@ public class WhiteRabbitRoomScript : MonoBehaviour
         background_1.SetActive(false);
         background_2.SetActive(false);
         background_3.SetActive(false);
+        background_4.SetActive(false);
+        background_5.SetActive(false);
     }
 
     public void SetBackgroundOneActive()
     {
         background_0.SetActive(false);
-        background_1.SetActive(true);
-        background_2.SetActive(false);
         background_3.SetActive(false);
 
+        if (!isAlice)
+        {
+            background_1.SetActive(true);
+            background_2.SetActive(false);
+        }
+        else {
+            background_4.SetActive(true);
+            background_5.SetActive(false);
+        }
     }
 
     public void SetBackgroundTwoActive()
     {
         background_0.SetActive(false);
-        background_1.SetActive(false);
-        background_2.SetActive(true);
         background_3.SetActive(false);
 
+        if (!isAlice)
+        {
+            background_1.SetActive(false);
+            background_2.SetActive(true);
+        }
+        else
+        {
+            background_4.SetActive(false);
+            background_5.SetActive(true);
+        }
     }
     public void SetBackgroundThreeActive()
     {
@@ -53,7 +83,8 @@ public class WhiteRabbitRoomScript : MonoBehaviour
         background_1.SetActive(false);
         background_2.SetActive(false);
         background_3.SetActive(true);
-    
+        background_4.SetActive(false);
+        background_5.SetActive(false);
         }
     }
 
