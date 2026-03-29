9bdcdc1c5f6bcb6fd8e9b464efbe711eb9855498
eldritchred <kaala.lily@gmail.com>
2026-03-29 19:20:33 -0400

    collider issues in tutorial are fixed for real this time

--- Assets/Scripts/PopupBackground.cs
+++ Assets/Scripts/PopupBackground.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 public class PopupBackground : MonoBehaviour
 {
     public GameObject background;
+    public GameObject door;
 
     private void Start()
     {
         background.SetActive(false);
     }
+
+    private void Update()
+    {
+        if (gameObject.activeSelf) { door.SetActive(false); }
+    }
+
     public void OnMouseDown()
     {
         if (gameObject.activeSelf)
         {
             background.SetActive(false);
+            door.SetActive(true);
         }
     }
 }
