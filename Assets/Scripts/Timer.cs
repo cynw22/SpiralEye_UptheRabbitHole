2b917cb0aff0a7c34c705f97742b086c0a57a008
cynw22 <cynthia.wang218@gmail.com>
2026-03-29 23:12:17 -0400

    Timer Edited

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,16 +5,22 @@ public class Timer : MonoBehaviour
 {
     public float time;
     public TextMeshProUGUI timeLeft;
+    public float seconds;
+    public float minutes;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Start()
     {
         time = 300;
+        seconds = 0;
+        minutes = 0;
     }
     // Update is called once per frame
     void Update()
     {
         time -= Time.deltaTime;
-        timeLeft.text = "Time Left: " + Mathf.Floor(time).ToString() + " seconds";
+        minutes = time / 60;
+        seconds = time % 60;
+        timeLeft.text = "Time Left: " + Mathf.Floor(minutes).ToString() + ":" + Mathf.Floor(seconds).ToString();
     }
 }
