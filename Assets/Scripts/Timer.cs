91cca16e1bac24fec99aed6122e4f1209cc2cf1a
cynw22 <cynthia.wang218@gmail.com>
2026-03-29 23:40:14 -0400

    Timer Works

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] float time;
+    [SerializeField] int LevelNumber;
     public TextMeshProUGUI timeLeft;
     public float seconds;
     public float minutes;
@@ -18,6 +20,16 @@ public class Timer : MonoBehaviour
     void Update()
     {
         time -= Time.deltaTime;
+
+        if (time <= 1) {
+            if (LevelNumber == 4) // 4 = White Rabbit House
+            {
+                SceneManager.LoadScene("ThePoster");
+            }
+            if (LevelNumber == 5) { // 5 = FindTheRose
+                SceneManager.LoadScene("FindTheRose"); //Change Later to Queens Court
+            }
+        }
         minutes = time / 60;
         seconds = time % 60;
         if (seconds >= 10) { 
