9d874820a01cb4c6400be0acc3233cb647a065ab
cynw22 <cynthia.wang218@gmail.com>
2026-03-31 12:36:48 -0400

    idk why briefcase not working ahh

---
+++ Assets/Scripts/Object Scripts/BreifcasePuzzle.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+// Getting text from an input field - https://discussions.unity.com/t/get-text-from-input-field-in-c/129605/6
+
+public class BreifcasePuzzle : MonoBehaviour
+{
+    public PuzzleControl4 puzzleControl;
+
+    private SpriteRenderer spriteRenderer;
+    public Sprite closeSprite;
+    public Sprite openSprite;
+
+    public LetterCycler[] letters;
+
+    [SerializeField] public GameObject cookies;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        cookies.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        checkPassword();
+    }
+
+    public void checkPassword()
+    {
+        int correctLetters = 0;
+        for (int i = 0; i < letters.Length; i++)
+        {
+            if (letters[i].spriteRenderer.sprite == puzzleControl.passcodeLetters[i])
+            {
+                correctLetters++;
+                Debug.Log("correct letters: " + correctLetters);
+            }
+        }
+
+        if (correctLetters == 5)
+        {
+            puzzleControl.suitcaseIsOpen = true;
+            spriteRenderer.sprite = openSprite;
+            cookies.SetActive(true);
+        }
+    }
+}
