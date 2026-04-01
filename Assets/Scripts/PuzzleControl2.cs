f768625b1400551cf1f8a1eb192a0f12a236dde6
cynw22 <cynthia.wang218@gmail.com>
2026-04-01 09:51:53 -0400

    FlowersWorking

--- Assets/Scripts/PuzzleControl2.cs
+++ Assets/Scripts/PuzzleControl2.cs
@@ -6,10 +6,26 @@ public class PuzzleControl2 : MonoBehaviour
 {
     public bool gumdrops, milk, flowers, boba, recipes, allItemsCollected = false;
 
+    //Books Popup
     GameObject booksNotTaken;
     GameObject booksTaken;
+
+    //Books Environment
     GameObject environmentTall;
     GameObject environmentShort;
+
+    //Flowers Environment
+    int numFlowers;
+    //GameObject[] flowersEnvironment;
+    GameObject flowersEnvironment0;
+    GameObject flowersEnvironment1;
+    GameObject flowersEnvironment2;
+    GameObject flowersEnvironment3;
+    GameObject flowersEnvironment4;
+    GameObject flowersEnvironment5;
+    GameObject flowersEnvironment6;
+    GameObject flowersEnvironment7;
+    GameObject flowersEnvironment8;
 
     void Start()
     {
@@ -20,6 +36,29 @@ public class PuzzleControl2 : MonoBehaviour
         environmentTall = GameObject.Find("Ch3_teaParty_Con_BG_bookStack_0");
         environmentShort = GameObject.Find("TinyBooksEnvironment");
         environmentShort.GetComponent<SpriteRenderer>().enabled = false;
+
+        //numFlowers = 8;
+        //thought maybe can do arrays but I dont understand arrays
+        //flowersEnvironment = GameObject.Find(flowersEnvironment.ToString("b"+numFlowers));
+
+        flowersEnvironment0 = GameObject.Find("f0");
+        flowersEnvironment0.GetComponent<SpriteRenderer>().enabled = true;
+        flowersEnvironment1 = GameObject.Find("f1");
+        flowersEnvironment1.GetComponent<SpriteRenderer>().enabled = true;
+        flowersEnvironment2 = GameObject.Find("f2");
+        flowersEnvironment2.GetComponent<SpriteRenderer>().enabled = true;
+        flowersEnvironment3 = GameObject.Find("f3");
+        flowersEnvironment3.GetComponent<SpriteRenderer>().enabled = true;
+        flowersEnvironment4 = GameObject.Find("f4");
+        flowersEnvironment4.GetComponent<SpriteRenderer>().enabled = true;
+        flowersEnvironment5 = GameObject.Find("f5");
+        flowersEnvironment5.GetComponent<SpriteRenderer>().enabled = true;
+        flowersEnvironment6 = GameObject.Find("f6");
+        flowersEnvironment6.GetComponent<SpriteRenderer>().enabled = true;
+        flowersEnvironment7 = GameObject.Find("f7");
+        flowersEnvironment7.GetComponent<SpriteRenderer>().enabled = true;
+        flowersEnvironment8 = GameObject.Find("f8");
+        flowersEnvironment8.GetComponent<SpriteRenderer>().enabled = true;
     }
 
     public void books_OnClick()
@@ -35,6 +74,18 @@ public class PuzzleControl2 : MonoBehaviour
 
     }
 
+    public void flowersCollected() {
+        flowersEnvironment0.GetComponent<SpriteRenderer>().enabled = false;
+        flowersEnvironment1.GetComponent<SpriteRenderer>().enabled = false;
+        flowersEnvironment2.GetComponent<SpriteRenderer>().enabled = false;
+        flowersEnvironment3.GetComponent<SpriteRenderer>().enabled = false;
+        flowersEnvironment4.GetComponent<SpriteRenderer>().enabled = false;
+        flowersEnvironment5.GetComponent<SpriteRenderer>().enabled = false;
+        flowersEnvironment6.GetComponent<SpriteRenderer>().enabled = false;
+        flowersEnvironment7.GetComponent<SpriteRenderer>().enabled = false;
+        flowersEnvironment8.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -43,6 +94,10 @@ public class PuzzleControl2 : MonoBehaviour
         if (recipes) {
             books_OnClick();
             booksEnvironment();
+        }
+
+        if (flowers) {
+            flowersCollected();
         }
     }
 
