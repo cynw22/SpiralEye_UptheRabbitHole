4386443aee6a21dede82839d181759ff0a7cc18d
nomba38 <noe38concepts@gmail.com>
2026-04-01 11:32:16 -0400

    particles fixed

--- Assets/Scripts/CursorChange.cs
+++ Assets/Scripts/CursorChange.cs
@@ -14,7 +14,6 @@ public class CursorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     public Sprite constanceClick;
 
     private Image image;
-
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
