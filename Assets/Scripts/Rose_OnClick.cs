c7553f9d0d019749ee26bbb70f82f9f33e86376c
nomba38 <noe38concepts@gmail.com>
2026-04-02 19:14:44 -0400

    rose fixes

--- Assets/Scripts/Rose_OnClick.cs
+++ Assets/Scripts/Rose_OnClick.cs
@@ -6,7 +6,7 @@ public class Rose_OnClick : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnMouseDown()
     {
-        SceneManager.LoadScene("GardenParty");
         Debug.Log("this works the scene just isn't in yet");
+        SceneManager.LoadScene("GardenParty");
     }
 }
