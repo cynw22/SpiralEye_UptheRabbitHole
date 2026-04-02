9e3e120f014ac35c5fc238498653dfc51aef952d
nomba38 <noe38concepts@gmail.com>
2026-04-02 19:04:42 -0400

    quick rose fix

--- Assets/Scripts/Rose_OnClick.cs
+++ Assets/Scripts/Rose_OnClick.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class Rose_OnClick : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void onClick()
+    void OnMouseDown()
     {
         SceneManager.LoadScene("GardenParty");
         Debug.Log("this works the scene just isn't in yet");
