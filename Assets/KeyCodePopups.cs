df4e3dcf85614ebb48480c3f1683873ff55e9629
cynw22 <cynthia.wang218@gmail.com>
2026-04-02 17:36:13 -0400

    Eat me drink me key popup works

--- Assets/KeyCodePopups.cs
+++ Assets/KeyCodePopups.cs
@@ -2,15 +2,62 @@ using UnityEngine;
 
 public class KeyCodePopups : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("References")]
+    public PuzzleControl1 puzzleManager; // The script that has the booleans
+    public GameObject missingCookiePopup; // The UI Panel to turn on
+    public GameObject missingBottlePopup; // The UI Panel to turn on
+    public GameObject missingBothPopup; // The UI Panel to turn on
 
-    // Update is called once per frame
+    // This prevents the popup from flicking back on after you close it
+    private bool hasBeenClosed = false;
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) { 
+            // 1. If the player already clicked 'Close', stop doing anything.
+            if (hasBeenClosed) return;
+
+        // 2. Check the conditions
+        // This triggers if chest is false AND if bottles is false
+        if (!puzzleManager.chestIsOpen && puzzleManager.allBottlesFound)
+        {
+            ShowCookiePopup();
+        }
+        if (puzzleManager.chestIsOpen && !puzzleManager.allBottlesFound)
+        {
+            ShowBottlePopup();
+        }
+
+        if (!puzzleManager.chestIsOpen && !puzzleManager.allBottlesFound)
+        {
+            ShowBothPopup();
+        }
+        }
     }
+
+    void ShowBothPopup()
+    {
+        missingBothPopup.SetActive(true);
+    }
+
+    void ShowCookiePopup()
+    {
+        missingCookiePopup.SetActive(true);
+    }
+    void ShowBottlePopup()
+    {
+        missingBottlePopup.SetActive(true);
+    }
+
+    // IMPORTANT: Link your UI Button "On Click()" to this function
+    public void ClosePopup()
+    {
+        missingBothPopup.SetActive(false);
+        missingBottlePopup.SetActive(false);
+        missingCookiePopup.SetActive(false);
+
+        // This is the key: it tells Update() to stop forcing the popup open
+        hasBeenClosed = true;
+    }
+
 }
