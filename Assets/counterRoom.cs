9c7b6487c0e659b1962d920d0450e236c4e959ea
cynw22 <cynthia.wang218@gmail.com>
2026-04-04 21:20:27 -0400

    BreifCaseUpdated

--- Assets/counterRoom.cs
+++ Assets/counterRoom.cs
@@ -4,10 +4,14 @@ public class counterRoom : MonoBehaviour
 {
     [SerializeField]PuzzleControl4 puzzleControl;
     private int checkCount;
+    SpriteRenderer thisObject;
+    [SerializeField] Sprite SwitchTo;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         checkCount = 0;
+        thisObject = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -17,6 +21,9 @@ public class counterRoom : MonoBehaviour
             puzzleControl.escapeRoomsComplete++;
             checkCount = 2;
             Debug.Log("Clicked on click count =2 ");
+            if (SwitchTo) {
+                thisObject.sprite = SwitchTo;
+            }
         }
         Debug.Log("Clicked on != 0");
     }
