bfd06c2f738c060a4e0500a28f2a733813ce2af1
eldritchred <kaala.lily@gmail.com>
2026-04-06 10:22:38 -0400

    various white rabbit newspaper fixes

--- Assets/Scripts/Object Scripts/Drawers.cs
+++ Assets/Scripts/Object Scripts/Drawers.cs
@@ -5,6 +5,8 @@ public class Drawers : MonoBehaviour
     [SerializeField] public GameObject newsp;
     public SpriteRenderer spriteRenderer;
 
+    private bool newsFound = false;
+
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -15,6 +17,11 @@ public class Drawers : MonoBehaviour
     private void OnMouseDown()
     {
         spriteRenderer.enabled = !spriteRenderer.enabled;
-        newsp.SetActive(spriteRenderer.enabled ? true : false);
+
+        if (!newsFound)
+        {
+            newsp.SetActive(spriteRenderer.enabled ? true : false);
+            newsFound = true;
+        }
     }
 }
--- Assets/Scripts/Object Scripts/Newspaper.cs
+++ Assets/Scripts/Object Scripts/Newspaper.cs
@@ -4,15 +4,18 @@ public class Newspaper : MonoBehaviour
 {
     [SerializeField] public PuzzleControl4 puzzleControl;
     private Collider2D col;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         col = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnMouseDown()
     {
         puzzleControl.escapeRoomsComplete++;
         col.enabled = false;
+        spriteRenderer.enabled = false;
     }
 }
