5b9affc44a3b2b958125710b4cca2b710394f10f
cynw22 <cynthia.wang218@gmail.com>
2026-04-10 20:36:47 -0400

    Figured out controls

---
+++ Assets/ControlsCode.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ControlsCode : MonoBehaviour
+{
+    [SerializeField] GameObject controlsPanel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controlsPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetActive() { 
+        controlsPanel.SetActive(true);
+    }
+    public void SetFalse()
+    { controlsPanel.SetActive(false);}
+}
