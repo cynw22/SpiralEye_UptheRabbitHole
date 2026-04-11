1ce73c4ef3901aad8d531073d3f2cf7de410b5af
cynw22 <cynthia.wang218@gmail.com>
2026-04-10 21:32:12 -0400

    Ending for FindTheRose Complete

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -13,6 +13,7 @@ public class Timer : MonoBehaviour
 
     [SerializeField] PuzzleControl5 PuzzleControl5;
     [SerializeField] PuzzleControl4 PuzzleControl4;
+    [SerializeField] GameObject toDisplay;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Start()
@@ -24,6 +25,7 @@ public class Timer : MonoBehaviour
             isCollected = PuzzleControl5.allObjectsFound;
             time = PuzzleControl5.time;
             LevelNumber = PuzzleControl5.levelNumber;
+            toDisplay.SetActive(false);
         }
         else if (PuzzleControl4)
         {
@@ -36,32 +38,51 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(time);
-        time -= Time.deltaTime;
-
-        //if ((time <= 1) && (!isCollected)) {
-        //    if (LevelNumber == 4) // 4 = White Rabbit House, Check if all the objects collected
-        //    {
-        //        SceneManager.LoadScene("ThePoster");
-        //    }
-        //    if (LevelNumber == 5) { // 5 = FindTheRose, Check if all the Roses Collected
-        //        SceneManager.LoadScene("FindTheRose"); //Change Later to Queens Court
-        //    }
-        //}
-
-        minutes = time / 60;
-        seconds = time % 60;
-        if (seconds >= 10) { 
-            timeLeft.text = "" + Mathf.Floor(minutes).ToString() + ":" + Mathf.Floor(seconds).ToString();
+        if (PuzzleControl5)
+        {
+            isCollected = PuzzleControl5.allObjectsFound;
+            time = PuzzleControl5.time;
+            LevelNumber = PuzzleControl5.levelNumber;
         }
-        else
-            timeLeft.text = "" + Mathf.Floor(minutes).ToString() +":0" + Mathf.Floor(seconds).ToString();
+        else if (PuzzleControl4)
+        {
+            isCollected = PuzzleControl4.allObjectsFound;
+            time = PuzzleControl4.time;
+            LevelNumber = PuzzleControl4.levelNumber;
+        }
+        //Debug.Log(time);
+        //time -= Time.deltaTime;
 
-        if (time <= 1) {
-            timeLeft.text = "0:00";
-            if (LevelNumber == -1) {
-                SceneManager.LoadScene("FindTheRose");//Remove later, this is for the poster
+        if ((time <= 1) && (isCollected == false))
+        {
+            if (LevelNumber == 4) // 4 = White Rabbit House, Check if all the objects collected
+            {
+                SceneManager.LoadScene("ThePoster");
+            }
+            if ((LevelNumber == 5) && (isCollected == false))
+            { // 5 = FindTheRose, Check if all the Roses Collected
+                //SceneManager.LoadScene("FindTheRoseTalk"); //Change Later to Queens Court
+                toDisplay.SetActive(true);
             }
         }
+
+            minutes = time / 60;
+            seconds = time % 60;
+            if (seconds >= 10)
+            {
+                timeLeft.text = "" + Mathf.Floor(minutes).ToString() + ":" + Mathf.Floor(seconds).ToString();
+            }
+            else
+                timeLeft.text = "" + Mathf.Floor(minutes).ToString() + ":0" + Mathf.Floor(seconds).ToString();
+
+            if (time <= 1)
+            {
+                timeLeft.text = "0:00";
+                if (LevelNumber == -1)
+                {
+                    SceneManager.LoadScene("ThePoster");//Remove later, this is for the poster
+                }
+            }
+        
     }
 }
